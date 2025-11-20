d6ba6f05b641933290d946f06bb0361ca6ab3c26
Ulas-Y <unalulasyalincak@gmail.com>
2025-11-20 20:28:36 +0300

    BASE TEMPLATES FOR ALL

---
+++ engine/utils/__init__.py
@@ -0,0 +1,5 @@
+from .config import EngineConfig
+from .math_utils import clamp, normalize_field
+from .plotting import plot_scalar_field
+
+__all__ = ["EngineConfig", "clamp", "normalize_field", "plot_scalar_field"]
